d62f7cac866f81d3c516ee9996790849abf2c18d
nexus-ramin <99713633+nexus-ramin@users.noreply.github.com>
2023-02-17 00:23:12 +0500

    Update caesar_cipher.py

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -1,6 +1,6 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
@@ -21,14 +21,9 @@ def encrypt(plain_text, shift_amount):
             cipher_text += letter
     # Return the encrypted text
     return cipher_text
-#TODO-1: Create a different function called 'decrypt' that takes the 'text' and 'shift' as inputs.
 
-  #TODO-2: Inside the 'decrypt' function, shift each letter of the 'text' *backwards* in the alphabet by the shift amount and print the decrypted text.  
-  #e.g. 
-  #cipher_text = "mjqqt"
-  #shift = 5
-  #plain_text = "hello"
-  #print output: "The decoded text is hello"
+#creating a decipher function
+
 def decrypt(plain_text, shift_amount):
     # Initialize an empty string to store the encrypted text
     decipher_text = ""
@@ -47,10 +42,10 @@ def decrypt(plain_text, shift_amount):
     # Return the encrypted text
     return decipher_text
 
-#TODO-3: Check if the user wanted to encrypt or decrypt the message by checking the 'direction' variable. Then call the correct function based on that 'drection' variable. You should be able to test the code to encrypt *AND* decrypt a message.
+#Check if the user wanted to encrypt or decrypt the message by checking the 'direction' variable. Then call the correct function based on that 'direction' variable.
 if direction == "encode":
   print(f"your encoded message is:\n{encrypt(plain_text=text, shift_amount=shift)}")
 elif direction == "decode":
   print(f"your decoded message is:\n{decrypt(plain_text=text, shift_amount=shift)}")
 else:
-  print("Please enter a valid form of operation")
+  print("Please enter a valid form of operation by typing wither encode or decode")
