4c5563d7c7fae7780bc652335741c3530d0f1682
nexus-ramin <99713633+nexus-ramin@users.noreply.github.com>
2023-02-16 23:57:51 +0500

    Update caesar_cipher.py

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -3,13 +3,22 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-#TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
 def encrypt(text, shift):
-  cipher_text =""
-  for letter in text:
-     position += alphabet.index(letter)
-     new_position = position + shift
-     cipher_text += alphabet[new_position]
-  print(cipher_text)
+    # Initialize an empty string to store the encrypted text
+    cipher_text = ""
+    for letter in text:
+        # Check if the letter is in the alphabet
+        if letter in alphabet:
+            # Get the index of the letter in the alphabet
+            position = alphabet.index(letter)
+            # Calculate the new position after shifting by the given amount
+            new_position = (position + shift) % len(alphabet)
+            # Add the shifted letter to the cipher text
+            cipher_text += alphabet[new_position]
+        else:
+            # Add non-letter characters to the cipher text as is
+            cipher_text += letter
+    # Return the encrypted text
+    return cipher_text
     
-encrypt(text, shift)
+print(encrypt(text, shift))
