6a2f1d8bbf635c4ebea84c62e2d7d7a226294442
nexus-ramin <99713633+nexus-ramin@users.noreply.github.com>
2023-02-16 23:54:06 +0500

    Add files via upload

---
+++ caesar_cipher.py
@@ -0,0 +1,15 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+# direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+#TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
+def encrypt(text, shift):
+  cipher_text =""
+  for letter in text:
+     position += alphabet.index(letter)
+     new_position = position + shift
+     cipher_text += alphabet[new_position]
+  print(cipher_text)
+    
+encrypt(text, shift)
