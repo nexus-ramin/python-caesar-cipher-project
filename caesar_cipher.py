b21edff12a22d8692317cdcafdf4a36440996639
nexus-ramin <99713633+nexus-ramin@users.noreply.github.com>
2023-02-17 00:20:59 +0500

    Update caesar_cipher.py

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -1,9 +1,10 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-# direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-def encrypt(text, shift):
+def encrypt(plain_text, shift_amount):
     # Initialize an empty string to store the encrypted text
     cipher_text = ""
     for letter in text:
@@ -20,5 +21,36 @@ def encrypt(text, shift):
             cipher_text += letter
     # Return the encrypted text
     return cipher_text
-    
-print(encrypt(text, shift))
+#TODO-1: Create a different function called 'decrypt' that takes the 'text' and 'shift' as inputs.
+
+  #TODO-2: Inside the 'decrypt' function, shift each letter of the 'text' *backwards* in the alphabet by the shift amount and print the decrypted text.  
+  #e.g. 
+  #cipher_text = "mjqqt"
+  #shift = 5
+  #plain_text = "hello"
+  #print output: "The decoded text is hello"
+def decrypt(plain_text, shift_amount):
+    # Initialize an empty string to store the encrypted text
+    decipher_text = ""
+    for letter in text:
+        # Check if the letter is in the alphabet
+        if letter in alphabet:
+            # Get the index of the letter in the alphabet
+            position = alphabet.index(letter)
+            # Calculate the new position after shifting by the given amount
+            new_position = (position - shift) % len(alphabet)
+            # Add the shifted letter to the cipher text
+            decipher_text += alphabet[new_position]
+        else:
+            # Add non-letter characters to the cipher text as is
+            decipher_text += letter
+    # Return the encrypted text
+    return decipher_text
+
+#TODO-3: Check if the user wanted to encrypt or decrypt the message by checking the 'direction' variable. Then call the correct function based on that 'drection' variable. You should be able to test the code to encrypt *AND* decrypt a message.
+if direction == "encode":
+  print(f"your encoded message is:\n{encrypt(plain_text=text, shift_amount=shift)}")
+elif direction == "decode":
+  print(f"your decoded message is:\n{decrypt(plain_text=text, shift_amount=shift)}")
+else:
+  print("Please enter a valid form of operation")
